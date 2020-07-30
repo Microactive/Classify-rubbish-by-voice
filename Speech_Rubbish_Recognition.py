34b9debbc847609f70c4a09962c8c17c4b569af2
xiexupang <41959142+xiexupang@users.noreply.github.com>
2020-07-30 18:31:42 +0800

    增加了通过GPIO控制LED灯

--- Speech_Rubbish_Recognition.py
+++ Speech_Rubbish_Recognition.py
@@ -9,6 +9,12 @@ import smbus
 import time
 import numpy
 import os
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)          #引脚采用BCM编号方式
+GPIO.setup([27, 5, 13, 26], GPIO.OUT)        #27、5、13、26号引脚设置为输出模式
+GPIO.output([27, 5, 13, 26], GPIO.LOW)       #初始化,27、5、13、26号引脚置低电平，默认状态下LED暗
 
 class ASR:
 
@@ -18,13 +24,10 @@ class ASR:
     
     ASR_RESULT_ADDR = 100   #识别结果存放处，通过不断读取此地址的值判断是否识别到语音，不同的值对应不同的语音
     
-
     ASR_WORDS_ERASE_ADDR = 101   #擦除所有词条
     
-
     ASR_MODE_ADDR = 102
     
-
     ASR_ADD_WORDS_ADDR = 160
     #词条添加的地址，支持掉电保存
 
@@ -50,9 +53,7 @@ class ASR:
             return -1        
         value = self.bus.read_byte(self.address)
         return value
-    
-    
-    
+        
     '''
     * 添加词条函数，
     * idNum：词条对应的识别号，1~255随意设置。识别到该号码对应的词条语音时，
@@ -81,6 +82,15 @@ class ASR:
            return False
         return True
         
+    def led(self, pin):   
+        count = 4
+        while (count > 0):
+            GPIO.output(pin, GPIO.HIGH)  #pin号引脚置高电平，此时LED灯被点亮
+            time.sleep(0.2)             #持续0.3s
+            GPIO.output(pin, GPIO.LOW)   #pin号引脚置低电平，此时LED灯熄灭
+            time.sleep(0.2)             #持续0.3s
+            count = count - 1
+        
 if __name__ == "__main__":
     addr = 0x79 #传感器iic地址
     asr = ASR(addr)
@@ -88,8 +98,7 @@ if __name__ == "__main__":
     #添加的词条和识别模式是可以掉电保存的，第一次设置完成后，可以将1改为0
     if 1:
         asr.eraseWords()
-        
-        
+               
         #setMode：识别模式设置，值范围1~3
         #1：循环识别模式。状态灯常亮（默认模式）    
         #2：口令模式，以第一个词条为口令。状态灯常灭，当识别到口令词时常亮，等待识别到新的语音,并且读取识别结果后即灭掉
@@ -146,33 +155,30 @@ if __name__ == "__main__":
         asr.addWords(5, 'ke hui shou wu')              # 可回收物  
         asr.addWords(5, 'you hai la ji')               # 有害垃圾 
         asr.addWords(5, 'qi ta la ji')                 # 其他垃圾 
-        
-        
+                
     while 1:
         data = asr.getResult()
         print("result:", data)
         
         if data == 1:   # 种类1：餐厨垃圾
             os.system("sudo mpg123 Kitchen_Waste.mp3")    # 播报Kitchen_Waste.mp3
-            #可在此处写代码控制外设，如舵机等
+            #可在此处写代码控制外设，如LED、舵机等
+            asr.led(27)   # 灯闪烁
             
         elif data == 2:    # 种类2：可回收物
             os.system("sudo mpg123 Recyclable_Waste.mp3")    # 播报Recyclable_waste.mp3
-            #可在此处写代码控制外设，如舵机等
+            #可在此处写代码控制外设，如LED、舵机等
+            asr.led(5)   # 灯闪烁
             
         elif data == 3:    # 种类3：有害垃圾
             os.system("sudo mpg123 Harmful_Waste.mp3")    # 播报Harmful_Waste.mp3
-            #可在此处写代码控制外设，如舵机等
+            #可在此处写代码控制外设，如LED、舵机等
+            asr.led(13)   # 灯闪烁
             
         elif data == 4:    # 种类4：其他垃圾
             os.system("sudo mpg123 Other_Waste.mp3")    # 播报Other_Waste.mp3
-            #可在此处写代码控制外设，如舵机等
+            #可在此处写代码控制外设，如LED、舵机等
+            asr.led(26)   # 灯闪烁
         else:
             print("无法识别该垃圾种类")
-        
-        
-        
-        
-        
-        
-        
+              
